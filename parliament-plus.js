eed03b6c02bd7a432845310022bbc6fcd83dba4d
Rowan Crawford <wombleton@gmail.com>
2010-11-18 02:44:58 +1300

    Refine, add auto-embedding.

--- parliament-plus.js
+++ parliament-plus.js
@@ -1,8 +1,11 @@
 (function($, undefined) {
   $(document).ready(function() {
-    $('a[href*=.pdf]').addClass('google-pdf').each(function() {
+    $('a[href*=.pdf]:has(cite)').addClass('google-pdf').each(function() {
       var $this = $(this),
+        iframe,
+        para,
         url = $this.attr('href'),
+        embedUrl,
         l = window.location;
       if (url.indexOf('/') === 0) {
         url = l.protocol + '//' + l.hostname + url;
@@ -11,6 +14,17 @@
       $this.attr('href', 'http://docs.google.com/viewer?' + $.param({
         url: url
       }));
+      embedUrl = 'http://docs.google.com/viewer?' + $.param({
+        url: url,
+        embedded: 'true'
+      });
+      para = $(".copy .section p:contains(et this document in PDF format from the ):last");
+      iframe = $('<iframe src="' + embedUrl + '" style="width:585px; height:500px;" frameborder="0"></iframe>');
+      if (para.length) {
+        iframe.insertAfter(para);
+      } else {
+        iframe.appendTo('.copy .section:first');
+      }
     });
   });
 }(jQuery));
