102cc8d91aae0919b54130071b4ef5a1de436d14
Rowan Crawford <wombleton@gmail.com>
2010-11-29 22:35:05 +1300

    add permalinks after any QWA/QOA link

--- parliament-plus.js
+++ parliament-plus.js
@@ -1,14 +1,25 @@
 (function($, undefined) {
+  function generateLink(key, text) {
+    return $('<span> <a title="permalink" href="' + getHref(key) + '">' + text + '</a> </span>');
+  }
+
+  function getHref(key) {
+    return 'http://www.parliament.nz/' + locale + '/?document=' + key;
+  }
 
   var doc = $(document),
-      width = doc.width();
+      width = doc.width(),
+      l = window.location,
+      locale = (l.pathname.match(/$\/([^\/])\/.+/) || [])[1] || 'en-NZ',
+      QWA_RE = new RegExp(locale + '.+/QOA/.+/([^/]+_[0-9]+_[0-9]+).+$'),
+      QOA_RE = new RegExp(locale + '.+/QWA/.+/([^/]+_[0-9]+_[0-9]+).+$');
 
   if (width >= 1280) {
     $('body').addClass('wide');
   }
   $(document).ready(function() {
 
-    $('a[href*=.pdf]:has(cite)').addClass('google-pdf').each(function() {
+    $('a[href*=.pdf]:has(cite)').each(function() {
       var $this = $(this),
         iframe,
         para,
@@ -21,17 +32,29 @@
 
       $this.attr('href', 'http://docs.google.com/viewer?' + $.param({
         url: url
-      }));
+      })).attr('target', '_blank');
       embedUrl = 'http://docs.google.com/viewer?' + $.param({
         url: url,
         embedded: 'true'
       });
       para = $(".copy .section p:contains(et this document in PDF format from the ):last");
-      iframe = $('<iframe src="' + embedUrl + '" style="width:585px; height:500px;" frameborder="0"></iframe>');
+      iframe = $('<iframe src="' + embedUrl + '" style="width:750px; height:500px;" frameborder="0"></iframe>');
       if (para.length) {
         iframe.insertAfter(para);
       } else {
         iframe.appendTo('.copy .section:first');
+      }
+    });
+
+    $('a').each(function() {
+      var $this = $(this),
+          href = $this.attr('href') || '',
+          key,
+          match = href.match(QOA_RE) || href.match(QWA_RE);
+
+      if (match) {
+        key = match[1];
+        generateLink(key, '#').insertAfter($this);
       }
     });
 
@@ -42,9 +65,10 @@
           match = text.match(re);
 
       if (match) {
-        $this.html(text.replace(re, '<a href="http://www.parliament.nz/en-NZ/?document=QWA_$2_$3">$1</a>'));
+        $this.html(text.replace(re, '<a href="http://www.parliament.nz/' + locale + '/?document=QWA_$2_$3">$1</a>'));
       }
     });
+
 
   });
 
